e88811065e8dc66e543b4bb29c34df54850f1020
danielmachadojm <danieldjm100@gmail.com>
2025-06-02 18:38:22 -0300

    Add files via upload

---
+++ Lista de Tarefas/src/modelo/Tarefa.java
@@ -0,0 +1,72 @@
+package modelo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import main.Main.ExportarComoPDF;
+
+@ExportarComoPDF
+public class Tarefa {
+    private String titulo;
+    private String descricao;
+    private Status status;
+    private Prioridade prioridade;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataConclusao;
+
+    public Tarefa(String titulo, String descricao, Prioridade prioridade) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prioridade = prioridade;
+        this.status = Status.PENDENTE;
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Prioridade getPrioridade() {
+        return prioridade;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(LocalDateTime dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + status + "] "
+                + descricao + " (Prioridade: " + prioridade
+                + ", Criada em: " + dataCriacao.toString() + ")";
+    }
+}
+
