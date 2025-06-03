17d598edea49909178a13921205f2ebe94fe6859
danielmachadojm <danieldjm100@gmail.com>
2025-06-03 00:53:23 -0300

    Delete Lista de Tarefas - POO directory

--- Lista de Tarefas - POO/annotations/PriorityLevel.java
+++
@@ -1,9 +0,0 @@
-package todoapp.annotations;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface PriorityLevel {
-    String[] allowedValues() default {"BAIXA", "MÉDIA", "ALTA"};
-}
